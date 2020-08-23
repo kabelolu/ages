3ce328c9ace4f5af600b5f3bfe4ea60b1baa080f
Kabelo Lucas Kekana <kabelolu86@gmail.com>
2020-08-23 08:22:01 +0200

    first commit

---
+++ Ages.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Ages{
+Ages(){}
+public static void main(String [] args){
+Scanner scan = new Scanner(System.in);
+int ages[] ={1,6,12,8,3,14,16,21,31,29,34,12,14};
+System.out.println("The sum of the ages is : " + (ages[0] + ages[1] + ages[2] + ages[3] + ages[4] + ages[5] + ages[6] + ages[7] + ages[8] + ages[9] + ages[10] + ages[11] +ages[12]));
+}
+}
